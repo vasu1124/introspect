dd16f051e3fbf6482c9db262585abd9d28596909
vasu1124 <vasu1124@actvirtual.com>
2020-04-26 20:58:12 +0200

    k14s kbld test

--- kubernetes/k14s/kapp-deploy.sh
+++ kubernetes/k14s/kapp-deploy.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+VERSION=v1.0
+COMMIT=`git rev-parse HEAD`
+BRANCH=`git rev-parse --abbrev-ref HEAD`
 
-kapp deploy -a introspect -f <(ytt -f .)
+kapp deploy -a introspect -f <(ytt template -v VERSION=$VERSION -v COMMIT=$COMMIT -v BRANCH=$BRANCH -f . | kbld -f -) --diff-changes
