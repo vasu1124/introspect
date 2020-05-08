88b369b34a1ba7ce51ec3ecbe11a1be29f246e27
vasu1124 <vasu1124@actvirtual.com>
2020-05-08 18:53:32 +0200

    use kbld lock file

--- kubernetes/k14s/kapp-deploy.sh
+++ kubernetes/k14s/kapp-deploy.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+dir="${0%/*}"
+cd $dir
+
 VERSION=v1.0
 COMMIT=`git rev-parse HEAD`
 BRANCH=`git rev-parse --abbrev-ref HEAD`
 
-kapp deploy -a introspect -f <(ytt template -v VERSION=$VERSION -v COMMIT=$COMMIT -v BRANCH=$BRANCH -f . | kbld -f -) --diff-changes
+kapp deploy -a introspect -f <(ytt template -v VERSION=$VERSION -v COMMIT=$COMMIT -v BRANCH=$BRANCH -f . | kbld --lock-output kbld.lock.yaml -f -) --diff-changes
