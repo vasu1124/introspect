12ce74bd75a7d2f2c9bda3c4fa0f2b03fe451aaf
vasu1124 <vasu1124@actvirtual.com>
2020-05-08 18:55:56 +0200

    no k8singress, issuer

--- kubernetes/istio/istio-install.sh
+++ kubernetes/istio/istio-install.sh
@@ -1,23 +1,14 @@
 #!/bin/bash
 
 export domainname="*.ingress.example.com"
+export issuer="garden"
 
 istioctl manifest apply --set profile=default \
   --set values.gateways.istio-ingressgateway.serviceAnnotations.'dns\.gardener\.cloud/dnsnames'=${domainname} \
   --set values.gateways.istio-ingressgateway.serviceAnnotations.'dns\.gardener\.cloud/ttl'='120' \
   --set values.gateways.istio-ingressgateway.serviceAnnotations.'dns\.gardener\.cloud/class'='garden' \
+  --set values.gateways.istio-ingressgateway.serviceAnnotations.'cert\.gardener\.cloud/issuer'=${issuer} \
+  --set values.gateways.istio-ingressgateway.serviceAnnotations.'cert\.gardener\.cloud/secretname'='wildcard-tls' \
   --set values.gateways.istio-ingressgateway.sds.enabled=true 
-
-# workaround: with istioctl 1.5.2 it seems you need to add k8sIngress incrementally.
-# this workaround breaks if any component is restarted.
-sleep 30
-
-istioctl manifest apply --set profile=default \
-  --set values.gateways.istio-ingressgateway.serviceAnnotations.'dns\.gardener\.cloud/dnsnames'=${domainname} \
-  --set values.gateways.istio-ingressgateway.serviceAnnotations.'dns\.gardener\.cloud/ttl'='120' \
-  --set values.gateways.istio-ingressgateway.serviceAnnotations.'dns\.gardener\.cloud/class'='garden' \
-  --set values.gateways.istio-ingressgateway.sds.enabled=true \
-  --set values.global.k8sIngress.enabled=true \
-  --set values.global.k8sIngress.enableHttps=true
 
 #  --set values.global.k8sIngress.gatewayName=ingressgateway
