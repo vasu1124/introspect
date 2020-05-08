93f71d83a30761cc5aed293f3e6ad7111a90b7e4
vasu1124 <vasu1124@actvirtual.com>
2020-05-08 18:45:50 +0200

    istio uninstall script

--- kubernetes/istio/istio-uninstall.sh
+++ kubernetes/istio/istio-uninstall.sh
@@ -2,6 +2,4 @@
 
 istioctl manifest generate --set profile=default \
   --set values.gateways.istio-ingressgateway.sds.enabled=true \
-  --set values.global.k8sIngress.enabled=true \
-  --set values.global.k8sIngress.enableHttps=true | \
-kubectl delete -f -
+| kubectl delete -f -
