d6c18f4d67eb70f43f03674e4999acfdc573a3b5
vasu1124 <vasu1124@actvirtual.com>
2021-01-18 15:37:02 +0100

    istio fresh (un)install

--- kubernetes/istio/istio-install.sh
+++ kubernetes/istio/istio-install.sh
@@ -3,21 +3,29 @@
 export domainname="*.ingress.example.com"
 export issuer="garden"
 
-istioctl manifest apply --set profile=default \
-  --set values.gateways.istio-ingressgateway.serviceAnnotations.'dns\.gardener\.cloud/dnsnames'=${domainname} \
-  --set values.gateways.istio-ingressgateway.serviceAnnotations.'dns\.gardener\.cloud/ttl'='120' \
-  --set values.gateways.istio-ingressgateway.serviceAnnotations.'dns\.gardener\.cloud/class'='garden' \
-  --set values.gateways.istio-ingressgateway.serviceAnnotations.'cert\.gardener\.cloud/issuer'=${issuer} \
-  --set values.gateways.istio-ingressgateway.serviceAnnotations.'cert\.gardener\.cloud/secretname'='wildcard-tls' \
-  --set values.gateways.istio-ingressgateway.sds.enabled=true 
-
-#  --set values.global.k8sIngress.gatewayName=ingressgateway
+cat <<EOF | istioctl install -y -f -
+apiVersion: install.istio.io/v1alpha1
+kind: IstioOperator
+spec:
+  profile: default
+  components:
+    ingressGateways:
+    - name: istio-ingressgateway
+      enabled: true
+      k8s:
+        serviceAnnotations:
+          dns.gardener.cloud/dnsnames: "${domainname}"
+          dns.gardener.cloud/ttl: "300" 
+          dns.gardener.cloud/class: garden
+          cert.gardener.cloud/issuer: "${issuer}"
+          cert.gardener.cloud/secretname: wildcard-tls
+EOF
 
 cat <<EOF | kubectl apply -f -
 apiVersion: networking.istio.io/v1beta1
 kind: Gateway
 metadata:
-  name: ${issuer}_gw
+  name: ${issuer}-gw
   namespace: istio-system
 spec:
   selector:
--- kubernetes/istio/istio-uninstall.sh
+++ kubernetes/istio/istio-uninstall.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-istioctl manifest generate --set profile=default \
-  --set values.gateways.istio-ingressgateway.sds.enabled=true \
-| kubectl delete -f -
+istioctl x uninstall --purge
